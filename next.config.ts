6e97e18b07424582a72fd086bb1a30fd7b79ca67
ParthSankpal <parth.sankpal47@gmail.com>
2025-12-11 17:06:56 +0530

    favicons in public file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-  domains: ["drive.google.com"],
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
